486fa2f873085ff1dc076edeab00dc014e8d1545
LuisEnriqueGarciaFernandez <luisenriqueg@gmail.com>
2012-04-07 18:33:14 +0200

    Primera versión del proyecto, sin desarrollar código.

---
+++ HolaMundoEGit/src/es/upm/dit/adsw/holaMundo/HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.upm.dit.adsw.holaMundo;
+
+/**
+ * @author luisenrique
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
